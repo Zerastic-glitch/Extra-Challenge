f1a36ca92249e0e62d2b5aa5efac1b533270c74e
Sven van den Berg <svthvdberg@gmail.com>
2021-05-07 17:11:02 +0200

    f

--- app/src/main/java/com/angelo/myapplication/Spel.kt
+++ app/src/main/java/com/angelo/myapplication/Spel.kt
@@ -1,14 +1,41 @@
 package com.angelo.myapplication
 
-class Spel (private val id : Int){
-    private val vlakken : Array<MetaVlak> = Array<MetaVlak>(9) { i -> MetaVlak() }
+class Spel (private val id : Int, private val spelers : Array<Speler>){
+    private val n
+    private val vlakken : Array<MetaVlak> = Array<MetaVlak>(n*n) { i -> MetaVlak() }
 
     init {
 
     }
 
     fun isGewonnen (): Speler?{
+        for (speler in spelers) {
+            if (isGewonnenDoorSpeler(speler)) {
+                return speler
+            }
+        }
         return null
+    }
+
+    fun isGewonnenDoorSpeler(speler:Speler): boolean {
+        endResDiag1 = true
+        endResDiag2 = true
+        for (i in 0..n-1) {
+            endResColom = true
+            endResRij = true
+            endResDiag1 = endResDiag1 && (vlakken[i * n + i].isGewonnen?.equals(speler))
+            endResDiag2 = endResDiag2 && (vlakken[i * n + 2-i].isGewonnen?.equals(speler))
+            for(b in 0..n-1) {
+                endResColom = endResColom && (vlakken[i * n + b].isGewonnen?.equals(speler))
+                endResRij = endResRij && (vlakken[b * n + i].isGewonnen?.equals(speler))
+            }
+            if (endResColom || endResRij) {
+                return true
+            }
+        }
+        if (endResDiag1 || endResDiag2) {
+            return true
+        }
     }
 
     fun nieuweBeurt(vlak : Vlak){
