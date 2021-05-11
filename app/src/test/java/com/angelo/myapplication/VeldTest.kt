870e850daddc29af0036984185d98f6e680e2313
Steven Mooij <s.mooij@hhs.nl>
2021-05-11 14:17:45 +0200

    Added test.

---
+++ app/src/test/java/com/angelo/myapplication/VeldTest.kt
@@ -0,0 +1,78 @@
+package com.angelo.myapplication
+
+import org.junit.Assert
+import org.junit.Test
+
+class VeldTest {
+    @Test
+    fun isGewonnenDoorSpeler() {
+        var speler1 = Speler()
+        var speler2 = Speler()
+        val spelers = arrayOf(speler1, speler2)
+        val vlakken: Array<Vlak> = Array<Vlak>(3 * 3) { i -> Vlak() }
+        vlakken[0].gekozenDoor = speler1;
+        vlakken[1].gekozenDoor = speler1;
+        vlakken[2].gekozenDoor = speler1;
+        Assert.assertTrue(Veld.isGewonnenDoorSpeler(speler1, spelers, vlakken, 3))
+    }
+
+    @Test
+    fun isGewonnenDoorSpeler2() {
+        var speler1 = Speler()
+        var speler2 = Speler()
+        val spelers = arrayOf(speler1, speler2)
+        val vlakken: Array<Vlak> = Array<Vlak>(3 * 3) { i -> Vlak() }
+        vlakken[0].gekozenDoor = speler1;
+        vlakken[3].gekozenDoor = speler1;
+        vlakken[6].gekozenDoor = speler1;
+        Assert.assertTrue(Veld.isGewonnenDoorSpeler(speler1, spelers, vlakken, 3))
+    }
+
+    @Test
+    fun isGewonnenDoorSpeler6() {
+        var speler1 = Speler()
+        var speler2 = Speler()
+        val spelers = arrayOf(speler1, speler2)
+        val vlakken: Array<Vlak> = Array<Vlak>(3 * 3) { i -> Vlak() }
+        vlakken[1].gekozenDoor = speler1;
+        vlakken[4].gekozenDoor = speler1;
+        vlakken[7].gekozenDoor = speler1;
+        Assert.assertTrue(Veld.isGewonnenDoorSpeler(speler1, spelers, vlakken, 3))
+    }
+
+    @Test
+    fun isGewonnenDoorSpeler3() {
+        var speler1 = Speler()
+        var speler2 = Speler()
+        val spelers = arrayOf(speler1, speler2)
+        val vlakken: Array<Vlak> = Array<Vlak>(3 * 3) { i -> Vlak() }
+        vlakken[0 + 0].gekozenDoor = speler1;
+        vlakken[3 + 1].gekozenDoor = speler1;
+        vlakken[6 + 2].gekozenDoor = speler1;
+        Assert.assertTrue(Veld.isGewonnenDoorSpeler(speler1, spelers, vlakken, 3))
+    }
+
+    @Test
+    fun isGewonnenDoorSpeler4() {
+        var speler1 = Speler()
+        var speler2 = Speler()
+        val spelers = arrayOf(speler1, speler2)
+        val vlakken: Array<Vlak> = Array<Vlak>(3 * 3) { i -> Vlak() }
+        vlakken[0 + 2].gekozenDoor = speler1;
+        vlakken[3 + 1].gekozenDoor = speler1;
+        vlakken[6 + 0].gekozenDoor = speler1;
+        Assert.assertTrue(Veld.isGewonnenDoorSpeler(speler1, spelers, vlakken, 3))
+    }
+
+    @Test
+    fun isGewonnenDoorSpeler5() {
+        var speler1 = Speler()
+        var speler2 = Speler()
+        val spelers = arrayOf(speler1, speler2)
+        val vlakken: Array<Vlak> = Array<Vlak>(3 * 3) { i -> Vlak() }
+        vlakken[0].gekozenDoor = speler1;
+        vlakken[1].gekozenDoor = speler1;
+        //vlakken[2].gekozenDoor = speler1;
+        Assert.assertFalse(Veld.isGewonnenDoorSpeler(speler1, spelers, vlakken, 3))
+    }
+}
