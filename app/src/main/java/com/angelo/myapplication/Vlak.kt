29301a62c791dd04dcda0fd69aca01f4302e173e
Angelo Heringa <48793053+AngeloHeringa@users.noreply.github.com>
2021-06-01 15:44:03 +0200

    isvalide en (meta)vlak constructor wijziging

--- app/src/main/java/com/angelo/myapplication/Vlak.kt
+++ app/src/main/java/com/angelo/myapplication/Vlak.kt
@@ -1,6 +1,6 @@
 package com.angelo.myapplication
 
-class Vlak () : Winbaar{
+class Vlak (val metaVlak : MetaVlak, var positie: Int) : Winbaar{
     var gekozenDoor : Speler? = null
     override fun isGewonnen(spelers : Array<Speler>, n : Int): Speler? {
         return gekozenDoor
